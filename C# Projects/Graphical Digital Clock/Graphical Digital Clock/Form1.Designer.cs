c7cec07cafa7f00349b4552896f626365c3eff7c
Ahmad Kandil <kandileng@gmail.com>
2016-07-02 17:56:09 +0200

    Graphical digital Clock

---
+++ C# Projects/Graphical Digital Clock/Graphical Digital Clock/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Graphical_Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 83);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(261, 103);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ C# Projects/Graphical Digital Clock/Graphical Digital Clock/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphical_Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        string path = @"C:\Users\Eng Ahmed Kandil\Desktop\Digits\";
+        Timer t = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_tick);
+            t.Start();
+        }
+        void t_tick(object sender, EventArgs args)
+        {
+            int[] time = {DateTime.Now.Hour/10,DateTime.Now.Hour%10,
+                          DateTime.Now.Minute/10,DateTime.Now.Minute%10,
+                           DateTime.Now.Second/10,DateTime.Now.Second%10};
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics Graphic = Graphics.FromImage(bmp);
+
+            for (int i = 0; i < time.Length; i++)
+            {
+                int extra = i/2*20;
+                Image img = Image.FromFile(path + time[i] + ".png");
+                Graphic.DrawImage(img, 10+i*32+extra, 10, 32, 46);
+                pictureBox1.Image = bmp;
+            }
+        }
+    }
+}
