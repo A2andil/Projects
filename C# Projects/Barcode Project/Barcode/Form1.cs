dff0a7dd387b4478cded862ae3015f6aa76d7037
Ahmad Kandil <kandileng@gmail.com>
2017-09-01 04:48:29 +0200

    Projects

---
+++ C# Projects/Barcode Project/Barcode/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zen.Barcode;
+
+namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Code128BarcodeDraw barcode = BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = barcode.Draw(textBox1.Text, 80);
+        }
+    }
+}
