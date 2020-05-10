77e2da00a9017f8bc36a9d110450179c6974f0cc
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-05-10 21:52:21 +0200

    Chess

--- C# Projects/10- Chess/Chess/Form1.cs
+++ C# Projects/10- Chess/Chess/Form1.cs
@@ -33,6 +33,9 @@ namespace Chess
 
         private List<Point> Moves = new List<Point>();
 
+        private int[] dx = new int[4] { 1, -1, 0, 0 };
+        private int[] dy = new int[4] { 0, 0, -1, 1 };
+
         public Chess()
         {
             InitializeComponent();
@@ -46,37 +49,128 @@ namespace Chess
                     board[i, j].Location = new Point(j * 70, i * 70);
                     board[i, j].Click += buttonClicked;
                     board[i, j].Cursor = Cursors.Hand;
-                    Controls.Add(board[i, j]);
+                    board[i, j].FlatStyle = FlatStyle.Flat;
+                    board[i, j].FlatAppearance.BorderSize = 1;
                     if ((i + j) % 2 == 0) board[i, j].BackColor = Color.BurlyWood;
                     else board[i, j].BackColor = Color.White;
+                    board[i, j].FlatAppearance.BorderColor = board[i, j].BackColor;
+                    Controls.Add(board[i, j]);
                 }
             }
             resize();
             place();
         }
 
+        private bool safe(int row, int col)
+            => row < 8 && col < 8 && row >= 0 && col >= 0;
+
         private void buttonClicked(object sender, EventArgs e)
         {
-
             Button btn = (Button)sender;
             int col = btn.Left / 70, row = btn.Top / 70;
             Moves.Clear();
             for (int i = 0; i < 8; i++)
                 for (int j = 0; j < 8; j++)
-                    if (boardValues[i, j] == -1)
-                        board[i, j].Text = "";
+                {
+                    board[i, j].BackColor
+                        = (i + j) % 2 == 0 ? Color.BurlyWood : Color.White;
+                    board[i, j].FlatAppearance.BorderColor = board[i, j].BackColor;
+                }
+            int fRow;
             switch (boardValues[row, col])
             {
                 case 0:
-                    int fRow = boardValues[row + 1, col];
+                    fRow = boardValues[row + 1, col];
                     if (fRow == -1)
                         Moves.Add(new Point(col, row + 1));
                     if (row == 1 && boardValues[row + 2, col] == -1 && fRow == -1)
                         Moves.Add(new Point(col, row + 2));
+                    if (safe(row + 1, col + 1) && boardValues[row + 1, col + 1] > 5)
+                        Moves.Add(new Point(col + 1, row + 1));
+                    if (safe(row + 1, col - 1) && boardValues[row + 1, col - 1] > 5)
+                        Moves.Add(new Point(col - 1, row + 1));
+                    break;
+                case 6:
+                    fRow = boardValues[row - 1, col];
+                    if (fRow == -1)
+                        Moves.Add(new Point(col, row - 1));
+                    if (row == 6 && boardValues[row - 2, col] == -1 && fRow == -1)
+                        Moves.Add(new Point(col, row - 2));
+                    if (safe(row - 1, col - 1) && boardValues[row - 1, col - 1] < 6
+                        && boardValues[row - 1, col - 1] != -1)
+                        Moves.Add(new Point(col - 1, row - 1));
+                    if (safe(row - 1, col + 1) && boardValues[row - 1, col + 1] < 6
+                        && boardValues[row - 1, col + 1] != -1)
+                        Moves.Add(new Point(col + 1, row - 1));
+                    break;
+                case 3:
+                case 9:
+                    for (int i = 0; i < dx.Length; i++)
+                    {
+                        int cl = col, rw = row;
+                        while (safe(cl + dx[i], rw + dy[i]))
+                        {
+                            cl += dx[i]; rw += dy[i];
+                            Moves.Add(new Point(cl, rw));
+                        }
+                    }
+                    break;
+                case 1:
+                case 7:
+                    int[] dk = new int[2] { 1, -1 };
+                    for (int i = 0; i < dk.Length; i++)
+                        for (int j = 0; j < dk.Length; j++)
+                        {
+                            int cl = col, rw = row;
+                            while (safe(cl + dk[i], rw + dk[j]))
+                            {
+                                cl += dk[i]; rw += dk[j];
+                                Moves.Add(new Point(cl, rw));
+                            }
+                        }
+                    break;
+                case 2:
+                case 8:
+                    int[] dh = new int[4] { -1, 1, -2, 2 };
+                    for (int i = 0; i < dh.Length; i++)
+                    {
+                        int comp = 3 - Math.Abs(dh[i]);
+                        if (safe(row + comp, col + dh[i]))
+                            Moves.Add(new Point(col + dh[i], row + comp));
+                        if (safe(row - comp, col + dh[i]))
+                            Moves.Add(new Point(col + dh[i], row - comp));
+                    }
+                    break;
+                case 4:
+                case 10:
+                    for (int i = 0; i < dx.Length; i++)
+                    {
+                        int cl = col, rw = row;
+                        while (safe(cl + dx[i], rw + dy[i]))
+                        {
+                            cl += dx[i]; rw += dy[i];
+                            Moves.Add(new Point(cl, rw));
+                        }
+                    }
+                    int[] dd = new int[2] { 1, -1 };
+                    for (int i = 0; i < dd.Length; i++)
+                        for (int j = 0; j < dd.Length; j++)
+                        {
+                            int cl = col, rw = row;
+                            while (safe(cl + dd[i], rw + dd[j]))
+                            {
+                                cl += dd[i]; rw += dd[j];
+                                Moves.Add(new Point(cl, rw));
+                            }
+                        }
                     break;
             }
             for (int i = 0; i < Moves.Count; i++)
-                board[Moves[i].Y, Moves[i].X].Text = "x";
+            {
+                board[Moves[i].Y, Moves[i].X].BackColor = Color.Coral;
+                board[Moves[i].Y, Moves[i].X].FlatAppearance.BorderColor 
+                    = Color.Coral;
+            }
         }
 
         private void place()
@@ -90,18 +184,35 @@ namespace Chess
                 boardValues[6, j] = 6;
                 board[6, j].Image = res[6];
             }
+
             //Rook
             board[0, 0].Image = board[0, 7].Image = res[3];
+            boardValues[0, 0] = boardValues[0, 7] = 3;
+
             board[7, 0].Image = board[7, 7].Image = res[9];
+            boardValues[7, 0] = boardValues[7, 7] = 9;
+
             //Knight
             board[0, 1].Image = board[0, 6].Image = res[1];
+            boardValues[0, 1] = boardValues[0, 6] = 1;
+
             board[7, 1].Image = board[7, 6].Image = res[7];
+            boardValues[7, 1] = boardValues[7, 6] = 7;
+
             //Horse
             board[0, 2].Image = board[0, 5].Image = res[2];
+            boardValues[0, 2] = boardValues[0, 5] = 2;
+
             board[7, 2].Image = board[7, 5].Image = res[8];
+            boardValues[7, 2] = boardValues[7, 5] = 8;
+
             //Queen
             board[0, 3].Image = res[4];
+            boardValues[0, 3] = 4;
+
             board[7, 3].Image = res[10];
+            boardValues[7, 3] = 10;
+
             //King
             board[0, 4].Image = res[5];
             board[7, 4].Image = res[11];
@@ -118,6 +229,11 @@ namespace Chess
                 graphic.Dispose();
                 res[i] = bmp;
             }
+        }
+
+        private void Chess_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
