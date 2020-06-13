e7669c05bd58fc1dc5306d28f0ecb4be61594233
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-06-13 13:30:23 +0200

    Flappy Bird

---
+++ C# Projects/12- Flappy Bird/12- Flappy Bird/FlappyBird.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _12__Flappy_Bird
+{
+    partial class FlappyBird
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PB = new System.Windows.Forms.PictureBox();
+            this.tm = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB
+            // 
+            this.PB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PB.Location = new System.Drawing.Point(0, 0);
+            this.PB.Name = "PB";
+            this.PB.Size = new System.Drawing.Size(700, 400);
+            this.PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB.TabIndex = 0;
+            this.PB.TabStop = false;
+            // 
+            // tm
+            // 
+            this.tm.Enabled = true;
+            this.tm.Interval = 15;
+            this.tm.Tick += new System.EventHandler(this.tm_Tick);
+            // 
+            // FlappyBird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 400);
+            this.Controls.Add(this.PB);
+            this.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "FlappyBird";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB;
+        private System.Windows.Forms.Timer tm;
+    }
+}
+
